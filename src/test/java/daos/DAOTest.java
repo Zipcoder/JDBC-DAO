25298c08ea4198469e2cbec6bb7965f58f1483d7
jeanvalentin51 <valentin.j.gheorghe.com>
2019-12-01 07:47:32 -0500

    Add test for find all

--- src/test/java/daos/DAOTest.java
+++ src/test/java/daos/DAOTest.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 public class DAOTest {
 
     DAO testDao;
@@ -24,6 +26,11 @@ public class DAOTest {
 
     @Test
     public void findAll() {
+        List<User> allUsers = testDao.findAll();
+        Integer expected = 10;
+        Integer actual = allUsers.size();
+
+        Assert.assertEquals(expected,actual);
     }
 
     @Test
